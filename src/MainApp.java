149084e1eeb1f60561107080ae601b838ccf818d
MikeDetroit <mikedetroit@gmail.com>
2018-05-10 13:12:45 -0400

    added a new line

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello, My name is Mike!");
+		
+		System.out.println("End of the line folks!");
 	}
 
 }
